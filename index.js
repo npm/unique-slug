b5c4f56401c3e37d5db4897b1c37fa02329a3d2d
Tobias Nießen <tniessen@tnie.de>
2018-09-01 19:20:32 +0200

    Use non-deprecated random API
    crypto.pseudoRandomBytes is deprecated on nodejs:master. This replaces
    the call to the deprecated API with crypto.randomBytes which is
    functionally equivalent.

--- index.js
+++ index.js
@@ -7,13 +7,8 @@ module.exports = function (uniq) {
     var hash = new MurmurHash3(uniq)
     return ('00000000' + hash.result().toString(16)).substr(-8)
   } else {
-    // Called without a callback, because this interface should neither block
-    // nor error (by contrast with randomBytes which will throw an exception
-    // without enough entropy).
-    //
-    // However, due to a change in Node 0.10.27+, pseudoRandomBytes is now the
-    // same as randomBytes, and may in fact block in situations where
-    // insufficent entropy is available.
-    return crypto.pseudoRandomBytes(4).toString('hex')
+    // Called without a callback, because this interface usually should neither
+    // block for a noticeable time nor error.
+    return crypto.randomBytes(4).toString('hex')
   }
 }
