f3fe9541b398f74e470403b0905c12804340dbfc
Luke Karrys <luke@lukekarrys.com>
2022-07-21 14:22:58 -0700

    chore: fix linting

--- test/index.js
+++ test/index.js
@@ -1,13 +1,13 @@
 'use strict'
 var t = require('tap')
-var uniqueSlug = require('../index.js')
+var uniqueSlug = require('../lib/index.js')
 
 t.plan(5)
 var slugA = uniqueSlug()
-t.is(slugA.length, 8, 'random slugs are 8 chars')
-t.notEqual(slugA, uniqueSlug(), "two slugs aren't the same")
+t.equal(slugA.length, 8, 'random slugs are 8 chars')
+t.not(slugA, uniqueSlug(), "two slugs aren't the same")
 var base = '/path/to/thingy'
 var slugB = uniqueSlug(base)
-t.is(slugB.length, 8, 'string based slugs are 8 chars')
-t.is(slugB, uniqueSlug(base), 'two string based slugs, from the same string are the same')
-t.notEqual(slugB, uniqueSlug(slugA), 'two string based slongs, from diff strings are different')
+t.equal(slugB.length, 8, 'string based slugs are 8 chars')
+t.equal(slugB, uniqueSlug(base), 'two string based slugs, from the same string are the same')
+t.not(slugB, uniqueSlug(slugA), 'two string based slongs, from diff strings are different')
